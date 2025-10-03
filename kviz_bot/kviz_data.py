5e2dee00538201a8e5762bf5d1b62b4706020f6d
Khasanov Almaz <130983039+Legodork13@users.noreply.github.com>
2025-10-03 17:58:57 +0500

    Add files via upload

---
+++ kviz_bot/kviz_data.py
@@ -0,0 +1,79 @@
+
+quiz_data = [
+    {
+        'question': 'Что такое Python?',
+        'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Как создать список в Python?',
+        'options': [
+            'list = [1, 2, 3]',
+            'list = (1, 2, 3)', 
+            'list = {1, 2, 3}',
+            'list = "1, 2, 3"'
+        ],
+        'correct_option': 0
+    },
+    {
+        'question': 'Что выведет код: print(2 ** 3)?',
+        'options': ['6', '8', '9', 'Ошибку'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Какой оператор используется для проверки равенства в Python?',
+        'options': ['=', '==', '===', 'equals'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Что такое "list comprehension" в Python?',
+        'options': [
+            'Способ создания списков',
+            'Метод чтения списков',
+            'Библиотека для работы со списками', 
+            'Тип данных'
+        ],
+        'correct_option': 0
+    },
+    {
+        'question': 'Как импортировать модуль в Python?',
+        'options': [
+            'import module_name',
+            'include module_name',
+            'require module_name',
+            'using module_name'
+        ],
+        'correct_option': 0
+    },
+    {
+        'question': 'Что делает функция len()?',
+        'options': [
+            'Возвращает длину объекта',
+            'Преобразует в строку',
+            'Округляет число',
+            'Вычисляет логарифм'
+        ],
+        'correct_option': 0
+    },
+    {
+        'question': 'Какой символ используется для комментариев в Python?',
+        'options': ['//', '#', '--', '/* */'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Что такое None в Python?',
+        'options': [
+            'Отсутствие значения',
+            'Пустая строка',
+            'Число 0',
+            'Ложное значение'
+        ],
+        'correct_option': 0
+    }
+    
+]
